d5daa2473f11dd6258c4370b5a97e618cd64c6e8
mukuru1 <kabengaedwin@gmail.com>
2026-01-18 21:59:14 +0200

    commit

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "@/App";
 import LandingPage from "@/pages/landingPage";
 import Login from "@/pages/login";
 import Dashboard from "@/pages/dashboard";
@@ -7,19 +8,25 @@ import ProtectedRoute from "@/components/protectedRoute";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <ProtectedRoute>
-        <Dashboard />
-      </ProtectedRoute>
-    ),
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: (
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
 ]);
 
